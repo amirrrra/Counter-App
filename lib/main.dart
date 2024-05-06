2bc81b8e5f0a6ca5876fba99967d59f10a6deb3c
Amira Tarek <amirat737@gmail.com>
2024-05-07 01:47:43 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:basketball_counter_app/views/home_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BasketballCounterApp());
+}
+
+class BasketballCounterApp extends StatelessWidget {
+  const BasketballCounterApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeView(),
+    );
+  }
+}
